f20103617dc9e7ad36e21bf3f08730c1f2a96ab9
Maria Diaz <mariangeles112@gmail.com>
2020-05-24 13:11:41 -0400

    Adding bootstrap and unit tests

---
+++ src/test/java/mdiaz/springboot/mdrecipes/services/RecipeServiceImplTest.java
@@ -0,0 +1,45 @@
+package mdiaz.springboot.mdrecipes.services;
+
+import static org.junit.Assert.*;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
+import java.util.HashSet;
+import java.util.Set;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import mdiaz.springboot.mdrecipes.domain.Recipe;
+import mdiaz.springboot.mdrecipes.repositories.RecipeRepository;
+
+public class RecipeServiceImplTest {
+
+	RecipeServiceImpl recipeService;
+
+	@Mock
+	RecipeRepository recipeRepository;
+
+	@Before
+	public void setUp() throws Exception {
+		MockitoAnnotations.initMocks(this);
+		recipeService = new RecipeServiceImpl(recipeRepository);
+	}
+
+	@Test
+	public void testGetRecipes() {
+		Recipe recipe = new Recipe();
+		HashSet<Recipe> recipesData = new HashSet<>();
+		recipesData.add(recipe);
+		
+		when(recipeService.getRecipes()).thenReturn(recipesData);
+		
+		Set<Recipe> recipes = recipeService.getRecipes();
+		assertEquals(recipes.size(), 1);
+		verify(recipeRepository, times(1)).findAll();
+	}
+
+}
